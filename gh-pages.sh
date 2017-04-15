39cd903890001ab535f5e76e0301087db55e938d
antonKalinin <hurricanebox@gmail.com>
2017-04-15 11:55:36 +0300

    Update files for GH Pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -7,7 +7,7 @@ git pull origin gh-pages
 npm run build
 npm run build-example
 cp dist/*.* .
-git add styles.css index.html index.js
+git add styles.css index.html bundle.js
 git commit -m 'Update gh-pages'
 git push origin gh-pages
 git checkout master
--- webpack.example.config.js
+++ webpack.example.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './example/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'index.js',
+        filename: 'bundle.js',
     },
     module: {
         rules: [
